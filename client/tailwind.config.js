5c4b91c6ff95d0c3d304442ea6d4dfe3d614a51c
christineiym <christineiymendoza@gmail.com>
2025-11-17 22:57:26 -0500

    buggy create-react-app

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        darkPurple: '#4A0080',
+        tcatTeal: '#29C5C0'
+      }
+    }
+  },
+  plugins: []
+}
